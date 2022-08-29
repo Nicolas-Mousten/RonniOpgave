0b6bfde3bf44567d791fb5ec7fff215827f2b63b
Nicolas-Mousten <nicolas.mousten@gmail.com>
2022-08-29 13:13:26 +0200

    Initial commit

---
+++ src/main/java/com/example/ronniopgave/RonniOpgaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.ronniopgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RonniOpgaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RonniOpgaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ronniopgave/RonniOpgaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ronniopgave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RonniOpgaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
